c4cd00afa6f997cb410af14c31bbcb6dfb18d22c
郭帅 <guoshuai@z-trip.cn>
2020-03-24 23:45:46 +0800

    1、 限制只能输入y或n来决定继续或终止 2、 限制答题数只能输入数字，并添加了提示文字 3、 获取答案方式优化以及空答案校验、历史答案展示

--- Project 1/adventure_game.py
+++ Project 1/adventure_game.py
@@ -1,7 +1,5 @@
 # 这个一个简单的文字游戏程序
-
-import time
-import random
+import time, random
 
 
 # 逐行打印间隔封装
@@ -24,11 +22,15 @@ def textShow():
 
 # 再来一次判断
 def agin():
-    agin = input('再玩一次？(y/n)\n').lower()
-    if agin == 'y' or 'none':
+    aginValue = input('再玩一次？(y/n)\n').lower()
+    if aginValue == 'y':
         getSelectNumber()
-    else:
+    elif aginValue == 'n':
         return textSleep("再来玩哦！", 0)
+    else:
+        isinstance (aginValue, str)
+        textSleep("请输入(y or n)", 0)
+        agin()
 
 
 # 获取用户选择的数字
@@ -36,18 +38,29 @@ def getSelectNumber():
     selectNumber = input("请输入你要挑战的问题数量\n") or 'none'
     # 如果直接按回车重新开始
     if selectNumber == 'none':
+        textSleep("输入的内容不能为空", 0)
         getSelectNumber()
-    else:
+    # 判断输入的是否是数字
+    elif selectNumber.isdigit():
         randomQuestion(int(selectNumber))
+    else:
+        textSleep("请输入数字", 0)
+        getSelectNumber()
 
 
 # 校验答案列表
 def answerCheck(answer, number):
-    if len(answer) > 0 and len(answer) == number:
-        textSleep("成功", 0)
+    # 检查是否存在空答案
+    flag = True
+    for item in answer:
+        if item == '':
+            flag = False
+    # 如果不存在空答案 and 答案数 == 问题数：通关 否则 失败
+    if flag and len(answer) == number:
+        textSleep("恭喜通关！:)", 0)
         agin()
     else:
-        textSleep("失败", 0)
+        textSleep("挑战失败！-.-", 0)
         agin()
 
 
@@ -66,7 +79,8 @@ def randomQuestion(number):
         answerItem = input(randomQuestionList[index]+'?\n')
         index += 1
         # 存储用户输入以供校验
-        answer += answerItem
+        answer.extend([answerItem])
+    print('你输入的所有答案：', answer)
     answerCheck(answer, number)
 
 
